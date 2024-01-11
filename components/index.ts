43aba42920ba0d2e854cfe44f115e2c007ce8bc2
Emeric Sailly <emeric.sailly@3ds.com>
2024-01-11 14:21:15 +0100

    Create CarCatalogue component

--- components/index.ts
+++ components/index.ts
@@ -2,4 +2,16 @@ import Hero from "./Hero";
 import CustomButton from "./CustomButton";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
-export { Hero, CustomButton, Navbar, Footer };
+import CarCatalogue from "./CarCatalogue";
+import Searchbar from "./Searchbar";
+import CustomFilter from "./CustomFilter";
+
+export {
+    Hero,
+    CustomButton,
+    Navbar,
+    Footer,
+    CarCatalogue,
+    Searchbar,
+    CustomFilter,
+};
